f49036ab9c8ddf6d13c567d449c0bfdf6b09dcf3
lilykr <lisa-lou.k@hotmail.fr>
2020-10-17 18:16:39 +0200

    added underlined links navbar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,19 +1,65 @@
 import { Link } from "gatsby"
 import React from "react"
 
+const arrLinkNavBar = [
+	{
+		name: "Accueil",
+		link: "/"
+	},
+
+	{
+		name: "Notre équipe",
+		link: "/equipe"
+	},
+
+	{
+		name: "Agréments",
+		link: "/agrements"
+	},
+
+	{
+		logo: true
+	},
+
+	{
+		name: "Process",
+		link: "/process"
+	},
+
+	{
+		name: "Champs d'intervention",
+		link: "/champs-d-intervention"
+	},
+
+	{
+		name: "Contact",
+		link: "/contact"
+	}
+
+]
+
+
 export default function Layout({ children, headTitle, headerText }) {
+
+
 	return (
 		<div className="layout">
 			<header className={children ? "" : "header-full-height"}>
 				<div className="nav-gradient">
 					<nav>
-						<Link to="/">Accueil</Link>
-						<Link to="/equipe">Notre équipe</Link>
-						<Link to="/agrements">Agréments</Link>
-						<img  className="logo-header" src="logo-fiducia.png" alt="Logo Fiducia" />
-						<Link to="/process">Process</Link>
-						<Link to="/champs-d-intervention">Champs d’intervention</Link>
-						<Link to="/contact">Contact</Link>
+						{arrLinkNavBar.map(element => {
+							if (element.logo) {
+								return <img className="logo-header" src="logo-fiducia.png" alt="Logo Fiducia" />
+							}
+							return <Link className={`link-header ${window.location.pathname === element.link ? "link-header-underlined" : "" }`} to={element.link}>{(element.name)}</Link>
+						})}
+						{/* <Link className="link-header" to="/">{arrLinkNavBar[0]}</Link>
+						<Link className="link-header" to="/equipe">{arrLinkNavBar[1]}</Link>
+						<Link className="link-header" to="/agrements">{arrLinkNavBar[2]}</Link>
+						<img className="logo-header" src="logo-fiducia.png" alt="Logo Fiducia" />
+						<Link className="link-header" to="/process">{arrLinkNavBar[3]}</Link>
+						<Link className="link-header" to="/champs-d-intervention">{arrLinkNavBar[4]}</Link>
+						<Link className="link-header" to="/contact">{arrLinkNavBar[5]}</Link> */}
 					</nav>
 				</div>
 				<div className="container">
@@ -27,8 +73,8 @@ export default function Layout({ children, headTitle, headerText }) {
 
 			</header>
 			{children ? (
-					<main>{children}</main>
-				) : null}
+				<main>{children}</main>
+			) : null}
 			<footer>
 				<div className="footer-elements-container">
 					<div className="elements-footer">
@@ -37,12 +83,12 @@ export default function Layout({ children, headTitle, headerText }) {
 					<br />06 86 14 21 74</div>
 					</div>
 					<div className="elements-footer">
-						<img className="logo-footer" src="/location.svg" style={{height: "25px"}} />
+						<img className="logo-footer" src="/location.svg" style={{ height: "25px" }} />
 						<div>41 rue Nicolas Bachelier
 					<br />31000 Toulouse </div>
 					</div>
 					<div className="elements-footer">
-						<img className="logo-footer" src="/mail.svg"  />
+						<img className="logo-footer" src="/mail.svg" />
 						<div>contact@fiducia-cf.com</div>
 					</div>
 				</div>
