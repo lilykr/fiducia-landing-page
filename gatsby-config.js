eaadf87566680b1415129197817605884889c8e3
lilykr <lisa-lou.k@hotmail.fr>
2022-01-18 11:06:23 +0100

    app builds with linkResolver prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const linkResolver = require('./linkResolver')
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   /* Your site config here */
   plugins: [
@@ -15,5 +22,17 @@ module.exports = {
         trackingId: "G-JFB389Q8NL",
       },
     },
+    // ...
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        linkResolver: (doc) => linkResolver(doc),
+      },
+    },
   ],
 }
+
