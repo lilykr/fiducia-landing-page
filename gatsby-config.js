c321de07c1e7ce0ab628ae662d2efb77d5bfff7c
lilykr <lisa-lou.k@hotmail.fr>
2022-01-20 12:16:55 +0100

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-graphql-config`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
